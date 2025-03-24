c588ffb88700533b9a5cde364c8258a256242240
Violet <136397110+Pirategirl9000@users.noreply.github.com>
2025-03-24 16:32:46 -0500

    Built The Basic structure

---
+++ src/Main.java
@@ -0,0 +1,47 @@
+/*
+
+• include your group member’s full names and repo URL in each class
+• using protected access modifier when appropriate
+• date field
+• 2 derived classes. Possible suggestions: Fish, Bird, etc.
+• 2 unique fields per derived class. Possible suggestions:
+• 1 or 2 enums applied at either the base or derived class level
+• Example at the base level:
+• HabitatType: CAGE, AQUARIUM, TERRARIUM, OPEN_SPACE
+• FeedingSchedule: ONCE_DAILY, TWICE_DAILY, THREE_TIMES_DAILY, WEEKLY,
+BIWEEKLY
+• Example at the derived class level: BodyShape, NestType, MigrationPattern
+• 2 packages
+• Package that stores the classes & enums related to inventory data
+• Package that stores the classes for managing the user’s interface
+• A user-friendly interface for managing the Pet’s store inventory
+which allows:
+• adding inventory
+• deleting inventory
+• display the inventory in a column structured list using printf
+• saving the inventory to a text file, using piped delimited
+• loading the inventory from a text file, using piped delimited
+• UML diagram as a JPG file (also checked into GitHub)
+• Optional (extra credit points):
+• 5 points = updating inventory
+• 5 points = properly written README.md
+• 5 points = properly written JavaDoc
+Do NOT try to code the whole project before testing. Incremental development is key!
+• Step 1: code and test the base class (optionally enum at the base level)
+• Step 2: code and test 1st of the derived classes (optionally enum at the derived level)
+• Step 3: code and test the PetStoreApp user-interface for only one of your inventory types
+• Step 4: code and test the 2nd derived class, enum, and update the PetStoreApp user-interface
+
+ */
+
+
+/*
+@Author: Violet French
+@src: https://github.com/Pirategirl9000/PetStoreApp
+ */
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/Pet.java
@@ -0,0 +1,11 @@
+/*
+@Author: Violet French
+@src: https://github.com/Pirategirl9000/PetStoreApp
+ */
+
+public class Pet {
+    enum HABITAT_TYPE {
+
+    }
+
+}
