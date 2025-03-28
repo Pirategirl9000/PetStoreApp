c0d079df05ebc35db917924e39f71eba5e5fe3eb
Violet <136397110+Pirategirl9000@users.noreply.github.com>
2025-03-27 23:57:38 -0500

    Added script headers

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,7 @@
 
 /*
-• include your group member’s full names and repo URL in each class
-• using protected access modifier when appropriate
 • date field
-• 2 derived classes. Possible suggestions: Fish, Bird, etc.
-• 2 unique fields per derived class.
-• A user-friendly interface for managing the Pet’s store inventory
-which allows:
-• adding inventory
-• display the inventory in a column structured list using printf
-• saving the inventory to a text file, using piped delimited
-• loading the inventory from a text file, using piped delimited
 • UML diagram as a JPG file (also checked into GitHub)
-
-
-• Optional (extra credit points):
-• 5 points = updating inventory
-• 5 points = properly written README.md
-• 5 points = properly written JavaDoc
  */
 
 
